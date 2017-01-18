28a4ea34b9ae2d38070b36bdda725b3d96baa924
yaoyoucai <2511022042@qq.com>
2017-01-18 21:37:10 +0800

    sss

---
+++ app/src/main/java/shbd/customview/view/PathDrawView.java
@@ -0,0 +1,24 @@
+package shbd.customview.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+/**
+ * Created by yh on 2017/1/18.
+ */
+
+public class PathDrawView extends View {
+    public PathDrawView(Context context) {
+        super(context);
+    }
+
+    public PathDrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public PathDrawView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
